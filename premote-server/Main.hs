61d57a6f65d63c13288d44602692613afe6a7e2b
Ricky Elrod <ricky@elrod.me>
2014-09-19 00:05:59 -0400

    scotty-tls boilerplate
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- premote-server/Main.hs
+++ premote-server/Main.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad.IO.Class
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
+import Web.Scotty
+import Web.Scotty.TLS
 
 -- | Send a key to a given window (on a given rootwindow, on a given display).
 sendKey :: Display -> Window -> Window -> KeyMask -> KeySym -> IO ()
@@ -15,7 +19,11 @@ sendKey display rootwindow window keymask keysym = allocaXEvent $ \event -> do
 
 main :: IO ()
 main = do
-  d <- openDisplay ""
-  rw <- rootWindow d $ defaultScreen d
-  sendKey d rw 39845910 noModMask xK_g
-  flush d
+  
+  scottyTLS 3000 "server.key" "server.crt" $ do
+    d <- liftIO $ openDisplay ""
+    rw <- liftIO $ rootWindow d $ defaultScreen d
+    get "/page-up" $ do
+      liftIO $ sendKey d rw 39845910 noModMask xK_g
+      liftIO $ flush d
+      text "done"
